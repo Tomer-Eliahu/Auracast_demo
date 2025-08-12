e2fbbb7a6be1f75b38b70dbceabed3b45e7540c6
Tomer Eliahu <73798700+Tomer-Eliahu@users.noreply.github.com>
2025-08-12 20:59:28 +0300

    adjustments

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
  */
 #include <stdint.h>
 
+#include <zephyr/kernel.h> //kerenl services
+
 #include <zephyr/bluetooth/gap.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
@@ -15,6 +17,9 @@
 #define TIMEOUT_SYNC_CREATE K_SECONDS(10)
 #define NAME_LEN            30
 
+///Change this to your Auracast Broadcast name
+#define BROADCAST_NAME "Tomer"
+
 static bool         per_adv_found;
 static bt_addr_le_t per_addr;
 static uint16_t per_adv_sync_timeout;
@@ -23,6 +28,8 @@ static uint8_t      per_sid;
 static K_SEM_DEFINE(sem_per_adv, 0, 1);
 static K_SEM_DEFINE(sem_per_sync, 0, 1);
 static K_SEM_DEFINE(sem_per_sync_lost, 0, 1);
+
+//static K_SEM_DEFINE(sem_auracast_found, 0, 1); //found our auracast broadcast
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
@@ -43,14 +50,35 @@ static void blink_timeout(struct k_work *work)
 }
 #endif
 
+//Results for high-quality broadcast are:
+// 23:08:02.347: discovered EXTENDED: BdAddr([74, 178, 179, 118, 40, 28]) and the direct addr is BdAddr([0, 0, 0, 0, 0, 0])
+// 23:08:02.347: The data is [6, 22, 82, 24, 58, 169, 91, 5, 22, 86, 24, 4, 0, 6, 48, 84, 111, 109, 101, 114]
+// 23:08:20.356: discovered EXTENDED: BdAddr([227, 22, 199, 247, 209, 3]) and the direct addr is BdAddr([0, 0, 0, 0, 0, 0])
+// 23:08:20.356: The data is [6, 22, 82, 24, 108, 141, 197, 5, 22, 86, 24, 4, 0, 6, 48, 84, 111, 109, 101, 114]
+// 17:49:01.722: discovered EXTENDED: BdAddr([30, 66, 139, 63, 217, 52]) and the direct addr is BdAddr([0, 0, 0, 0, 0, 0])
+// 17:49:01.722: The data is [6, 22, 82, 24, 7, 111, 34, 5, 22, 86, 24, 4, 0, 6, 48, 84, 111, 109, 101, 114]
+//                            ^               ^   ^    ^ I think these 3 bytes that vary might be the broadcast id.
+//                           Length, AD Type (22; 0x16 in hex)--Service Data - 16-bit UUID,
+// 
+
+
+/// @brief This is called on each Length Type Value triplet In a particular scan result.
+/// When it returns true the parsing continues. When it returns false the parsing stops.
+///
+
+/// 48 in hex is 0x30 which is Broadcast_Name in the Bluetooth assigned numbers.
+/// This is the only way for us to identify the Auracast broadcast 
+/// as the (broadcast source) device address is randomized.
+/// @param data 
+/// @param user_data 
+/// @return 
 static bool data_cb(struct bt_data *data, void *user_data)
 {
 	char *name = user_data;
 	uint8_t len;
 
 	switch (data->type) {
-	case BT_DATA_NAME_SHORTENED:
-	case BT_DATA_NAME_COMPLETE:
+	case BT_DATA_BROADCAST_NAME:
 		len = MIN(data->data_len, NAME_LEN - 1);
 		memcpy(name, data->data, len);
 		name[len] = '\0';
@@ -79,47 +107,50 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 
 	(void)memset(name, 0, sizeof(name));
 
+	//This function modifies name which is why we need to reset it above.
 	bt_data_parse(buf, data_cb, name);
+	
+	bool found_auracast= strcmp(name, BROADCAST_NAME);
+	
+	if (found_auracast) {
+		printk("Found AURACAST!\n");
+		
+		bt_addr_le_to_str(info->addr, le_addr, sizeof(le_addr));
+		printk("[DEVICE]: %s, AD evt type %u, Tx Pwr: %i, RSSI %i Broadcast name: %s "
+			"C:%u S:%u D:%u SR:%u E:%u Prim: %s, Secn: %s, "
+			"Interval: 0x%04x (%u ms), SID: %u\n",
+			le_addr, info->adv_type, info->tx_power, info->rssi, name,
+			(info->adv_props & BT_GAP_ADV_PROP_CONNECTABLE) != 0,
+			(info->adv_props & BT_GAP_ADV_PROP_SCANNABLE) != 0,
+			(info->adv_props & BT_GAP_ADV_PROP_DIRECTED) != 0,
+			(info->adv_props & BT_GAP_ADV_PROP_SCAN_RESPONSE) != 0,
+			(info->adv_props & BT_GAP_ADV_PROP_EXT_ADV) != 0,
+			phy2str(info->primary_phy), phy2str(info->secondary_phy),
+			info->interval, info->interval * 5 / 4, info->sid);
 
-	if (strlen(CONFIG_PER_ADV_NAME) > 0 && strcmp(name, CONFIG_PER_ADV_NAME) != 0) {
-		return;
-	}
+		if (!per_adv_found && info->interval) {
+			uint32_t interval_us;
+			uint32_t timeout;
 
-	bt_addr_le_to_str(info->addr, le_addr, sizeof(le_addr));
-	printk("[DEVICE]: %s, AD evt type %u, Tx Pwr: %i, RSSI %i %s "
-	       "C:%u S:%u D:%u SR:%u E:%u Prim: %s, Secn: %s, "
-	       "Interval: 0x%04x (%u ms), SID: %u\n",
-	       le_addr, info->adv_type, info->tx_power, info->rssi, name,
-	       (info->adv_props & BT_GAP_ADV_PROP_CONNECTABLE) != 0,
-	       (info->adv_props & BT_GAP_ADV_PROP_SCANNABLE) != 0,
-	       (info->adv_props & BT_GAP_ADV_PROP_DIRECTED) != 0,
-	       (info->adv_props & BT_GAP_ADV_PROP_SCAN_RESPONSE) != 0,
-	       (info->adv_props & BT_GAP_ADV_PROP_EXT_ADV) != 0,
-	       phy2str(info->primary_phy), phy2str(info->secondary_phy),
-	       info->interval, info->interval * 5 / 4, info->sid);
+			per_adv_found = true;
 
-	if (!per_adv_found && info->interval) {
-		uint32_t interval_us;
-		uint32_t timeout;
+			/* Add retries and convert to unit in 10's of ms */
+			interval_us = BT_GAP_PER_ADV_INTERVAL_TO_US(info->interval);
 
-		per_adv_found = true;
+			timeout = BT_GAP_US_TO_PER_ADV_SYNC_TIMEOUT(interval_us);
 
-		/* Add retries and convert to unit in 10's of ms */
-		interval_us = BT_GAP_PER_ADV_INTERVAL_TO_US(info->interval);
+			/* 10 attempts */
+			timeout *= 10;
 
-		timeout = BT_GAP_US_TO_PER_ADV_SYNC_TIMEOUT(interval_us);
+			/* Enforce restraints */
+			per_adv_sync_timeout =
+				CLAMP(timeout, BT_GAP_PER_ADV_MIN_TIMEOUT, BT_GAP_PER_ADV_MAX_TIMEOUT);
 
-		/* 10 attempts */
-		timeout *= 10;
+			per_sid = info->sid;
+			bt_addr_le_copy(&per_addr, info->addr);
 
-		/* Enforce restraints */
-		per_adv_sync_timeout =
-			CLAMP(timeout, BT_GAP_PER_ADV_MIN_TIMEOUT, BT_GAP_PER_ADV_MAX_TIMEOUT);
-
-		per_sid = info->sid;
-		bt_addr_le_copy(&per_addr, info->addr);
-
-		k_sem_give(&sem_per_adv);
+			k_sem_give(&sem_per_adv);
+		}
 	}
 }
 
